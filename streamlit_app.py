9be36d001bcc7157c6cde469aeabd7cf7c7e0c7b
swatantra21 <129853871+swatantra21@users.noreply.github.com>
2023-04-04 20:11:41 +0530

    Update streamlit_app.py
    Update title and added new header fruit smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title("My Mom's New Healthy Dinner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
